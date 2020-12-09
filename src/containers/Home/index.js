3c6df1a87b6d2c2f0377ddbc1e91df9f7af65550
Patrick Murphy <pmurphy@g.harvard.edu>
2020-12-09 05:04:41 -0800

    Update index.js

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Header, Grid, Icon } from 'semantic-ui-react';
+import { Header, Grid, Table } from 'semantic-ui-react';
 
 import { LOAD_PATIENT_INFO } from './constants';
 import { getReady } from './selectors';
@@ -16,21 +16,57 @@ class Home extends React.Component {
   }
 
   render() {
+    const { conditions } = this.props;
+
+    if (!conditions) {
+      return <p><i>Still loading...</i></p>;
+    }
+
     return (
       <React.Fragment>
         <Grid.Row>
           <Header as="h2">
-            <Icon name="check" />
-            Build something great
+            The final build:
           </Header>
         </Grid.Row>
+
+        <Grid.Row>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Condition</Table.HeaderCell>
+                <Table.HeaderCell>Code</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {conditions
+              .filter(condition => condition.code.coding)
+              .map(condition => {
+                return (
+                <Table.Row>
+                  <Table.Cell>(condition.code.coding[0].display)</Table.Cell>
+                  <Table.Cell>(condition.code.coding[0].code)</Table.Cell>
+                </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table>
+        </Grid.Row>
+
+
+
+
       </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return { ready: getReady(state), patient: getPatient(state) };
+const mapStateToProps = state => {
+  return { 
+    ready: getReady(state), 
+    patient: getPatient(state),
+    conditions: state.home.conditions, 
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
